9b5199255756ac25f02376e4d09d0e39e3c59cbe
Cooper Lees <cooper@fb.com>
2018-07-17 15:42:08 -0700

    Only define install_requires needed for respective Python versions
    Summary:
    - Lets only define the install requires for the Python versions needed
    - < 3 needs "futures", "ipaddress", "typing"
    - == 3 < 3.5 need typing
    
    I also auto formatted with black.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D8881822
    
    fbshipit-source-id: ae7b7339ffdd707dc41d86bb2cc7d6bf8282a087

--- openr/py/setup.py
+++ openr/py/setup.py
@@ -15,63 +15,63 @@ import os
 
 from setuptools import setup, find_packages
 from subprocess import check_call
+from sys import version_info
 
 
 def create_package_list(base):
-    '''
+    """
     Get all packages under the base directory
-    '''
+    """
 
-    return ([base] + ['{}.{}'.format(base, pkg) for pkg in find_packages(base)])
+    return [base] + ["{}.{}".format(base, pkg) for pkg in find_packages(base)]
 
 
 def generate_thrift_files():
-    '''
+    """
     Get list of all thrift files (absolute path names) and then generate
     python definitions for all thrift files.
-    '''
+    """
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    thrift_dir = os.path.join(os.path.dirname(current_dir), 'if')
-    thrift_files = [x for x in os.listdir(thrift_dir) if x.endswith('.thrift')]
+    thrift_dir = os.path.join(os.path.dirname(current_dir), "if")
+    thrift_files = [x for x in os.listdir(thrift_dir) if x.endswith(".thrift")]
 
     for thrift_file in thrift_files:
-        print('> Generating python definition for {}'.format(thrift_file))
-        check_call([
-            "thrift1",
-            "--gen", "py",
-            "--out", current_dir,
-            os.path.join(thrift_dir, thrift_file),
-        ])
+        print("> Generating python definition for {}".format(thrift_file))
+        check_call(
+            [
+                "thrift1",
+                "--gen",
+                "py",
+                "--out",
+                current_dir,
+                os.path.join(thrift_dir, thrift_file),
+            ]
+        )
 
 
 generate_thrift_files()
+
+# Have different install_requires for Python 2 + 3
+INSTALL_REQUIRES = ["bunch", "click", "hexdump", "networkx", "pyzmq", "six", "tabulate"]
+# Python 2 always needs these dependencies
+if version_info.major < 3:
+    INSTALL_REQUIRES.extend(["futures", "ipaddress", "typing"])
+# Only add typing before 3.5 for Python 3
+if version_info.major == 3 and version_info.minor < 5:
+    INSTALL_REQUIRES.append("typing")
+
 setup(
-    name='py-openr',
-    version='1.0',
-    author='Open Routing',
-    author_email='openr@fb.com',
+    name="py-openr",
+    version="1.0",
+    author="Open Routing",
+    author_email="openr@fb.com",
     description=(
-        'OpenR python tools and bindings. Includes python bindings for various '
-        'OpenR modules, CLI tool for interacting with OpenR named as `breeze`.'
+        "OpenR python tools and bindings. Includes python bindings for various "
+        "OpenR modules, CLI tool for interacting with OpenR named as `breeze`."
     ),
-    packages=create_package_list('openr'),
-    entry_points={
-        'console_scripts': [
-            'breeze=openr.cli.breeze:main',
-        ]
-    },
-    license='MIT License',
-    install_requires=[
-        'bunch',
-        'click',
-        'futures; python_version < "3"',
-        'hexdump',
-        'networkx',
-        'ipaddress',
-        'pyzmq',
-        'six',
-        'tabulate',
-        'typing',
-    ],
+    packages=create_package_list("openr"),
+    entry_points={"console_scripts": ["breeze=openr.cli.breeze:main"]},
+    license="MIT License",
+    install_requires=INSTALL_REQUIRES,
 )
