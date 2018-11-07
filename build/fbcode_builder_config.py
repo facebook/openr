d1f20cb8625e8120783413c3ef8166767d414fc1
Saif Hasan <has@fb.com>
2018-11-06 19:42:02 -0800

    format python code with Black linter
    Summary: For consistence looking codebase
    
    Reviewed By: cenzhao
    
    Differential Revision: D12947519
    
    fbshipit-source-id: 3b42ccd0103faf03bcc49628c756c881c1684508

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -7,67 +7,76 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-'fbcode_builder steps to build & test Openr'
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import specs.fbthrift as fbthrift
-import specs.folly as folly
 import specs.fbzmq as fbzmq
+import specs.folly as folly
 import specs.re2 as re2
+from shell_quoting import ShellQuoted, path_join
 
-from shell_quoting import path_join, ShellQuoted
+
+"fbcode_builder steps to build & test Openr"
 
 
 def fbcode_builder_spec(builder):
-    builder.add_option('thom311/libnl:git_hash', 'libnl3_2_25')
-    builder.add_option(
-        'openr/openr/build:cmake_defines', {'ADD_ROOT_TESTS': 'OFF'}
-    )
+    builder.add_option("thom311/libnl:git_hash", "libnl3_2_25")
+    builder.add_option("openr/openr/build:cmake_defines", {"ADD_ROOT_TESTS": "OFF"})
     maybe_curl_patch = []
     patch = path_join(
-        builder.option('projects_dir'),
-        '../shipit_projects/openr/build/fix-route-obj-attr-list.patch')
+        builder.option("projects_dir"),
+        "../shipit_projects/openr/build/fix-route-obj-attr-list.patch",
+    )
 
-    if not builder.has_option('shipit_project_dir'):
-        maybe_curl_patch = [builder.run(ShellQuoted(
-            'curl -O https://raw.githubusercontent.com/facebook/openr/master/'
-            'build/fix-route-obj-attr-list.patch'))]
-        patch = 'fix-route-obj-attr-list.patch'
+    if not builder.has_option("shipit_project_dir"):
+        maybe_curl_patch = [
+            builder.run(
+                ShellQuoted(
+                    "curl -O https://raw.githubusercontent.com/facebook/openr/master/"
+                    "build/fix-route-obj-attr-list.patch"
+                )
+            )
+        ]
+        patch = "fix-route-obj-attr-list.patch"
     libnl_build_commands = maybe_curl_patch + [
-        builder.run(ShellQuoted('git apply {p}').format(p=patch)),
-        builder.run(ShellQuoted('./autogen.sh')),
+        builder.run(ShellQuoted("git apply {p}").format(p=patch)),
+        builder.run(ShellQuoted("./autogen.sh")),
         builder.configure(),
-        builder.make_and_install()]
+        builder.make_and_install(),
+    ]
 
     return {
-        'depends_on': [folly, fbthrift, fbzmq, re2],
-        'steps': [
-            builder.github_project_workdir('thom311/libnl', '.'),
-            builder.step('Build and install thom311/libnl', libnl_build_commands),
-
-            builder.fb_github_project_workdir('openr/openr/build', 'facebook'),
-            builder.step('Build and install openr/openr/build', [
-                builder.cmake_configure('openr/openr/build'),
-                # we need the pythonpath to find the thrift compiler
-                builder.run(ShellQuoted(
-                    'PYTHONPATH="$PYTHONPATH:"{p}/lib/python2.7/site-packages '
-                    'make -j {n}'
-                ).format(p=builder.option('prefix'), n=builder.option('make_parallelism'))),
-                builder.run(ShellQuoted('make install')),
-            ]),
-            builder.step('Run openr tests', [
-                builder.run(
-                    ShellQuoted('CTEST_OUTPUT_ON_FAILURE=TRUE make test')
-                ),
-            ]),
+        "depends_on": [folly, fbthrift, fbzmq, re2],
+        "steps": [
+            builder.github_project_workdir("thom311/libnl", "."),
+            builder.step("Build and install thom311/libnl", libnl_build_commands),
+            builder.fb_github_project_workdir("openr/openr/build", "facebook"),
+            builder.step(
+                "Build and install openr/openr/build",
+                [
+                    builder.cmake_configure("openr/openr/build"),
+                    # we need the pythonpath to find the thrift compiler
+                    builder.run(
+                        ShellQuoted(
+                            'PYTHONPATH="$PYTHONPATH:"{p}/lib/python2.7/site-packages '
+                            "make -j {n}"
+                        ).format(
+                            p=builder.option("prefix"),
+                            n=builder.option("make_parallelism"),
+                        )
+                    ),
+                    builder.run(ShellQuoted("make install")),
+                ],
+            ),
+            builder.step(
+                "Run openr tests",
+                [builder.run(ShellQuoted("CTEST_OUTPUT_ON_FAILURE=TRUE make test"))],
+            ),
         ],
     }
 
 
 config = {
-    'github_project': 'facebook/openr',
-    'fbcode_builder_spec': fbcode_builder_spec,
+    "github_project": "facebook/openr",
+    "fbcode_builder_spec": fbcode_builder_spec,
 }
