2e537b7095818837fd5525fa58f5c31a6fae0891
Robert Copeland <bobcopeland@fb.com>
2019-06-21 05:56:38 -0700

    backout D15632816
    Summary:
    Back out "[soma][fbmeshd] netlink socket error NLE_MSG_OVERFLOW should exit"
    
    Original commit changeset: 2ec2f7d8d85d
    
    We should not exit if the kernel returns -EEXIST which can happen
    normally if a peer is already uploaded to the kernel.  Exiting causes
    fbmeshd to crashloop.
    
    Reviewed By: ilyacodesFB
    
    Differential Revision: D15936840
    
    fbshipit-source-id: 9a1d1e45fd26c89a30b13d55deff6a77b6236867

--- openr/fbmeshd/nl/NetlinkSocket.h
+++ openr/fbmeshd/nl/NetlinkSocket.h
@@ -13,7 +13,6 @@
 #include <glog/logging.h>
 
 #include <folly/Singleton.h>
-#include <folly/lang/Assume.h>
 
 #include <openr/fbmeshd/nl/NetlinkCallbackHandle.h>
 
@@ -89,10 +88,11 @@ class NetlinkSocket {
       }
 
       if (err->error == -NLE_MSG_OVERFLOW) {
-        LOG(FATAL) << "Kernel overflow error; crashing. ("
-                   << "command: " << static_cast<uint32_t>(gnlh->cmd)
-                   << "; sequence: " << err->msg.nlmsg_seq << ")";
-        folly::assume_unreachable();
+        LOG(WARNING)
+            << "Kernel overflow error; retrying receiving netlink message. ("
+            << "command: " << static_cast<uint32_t>(gnlh->cmd)
+            << "; sequence: " << err->msg.nlmsg_seq << ")";
+        return NL_SKIP;
       }
 
       throw std::runtime_error(
