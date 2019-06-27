577bdb688ec560c58fec856b5f4f150f92c6e759
Saif Hasan <has@fb.com>
2019-06-27 14:08:44 -0700

    add missing copyright header to cmake files
    Summary: As titled. OpenSource linter complained about this.
    
    Reviewed By: xiangxu1121
    
    Differential Revision: D16037266
    
    fbshipit-source-id: 8240c2e7e2b86d0ce202bc655adddf46016e108c

--- ThriftLibrary.cmake
+++ ThriftLibrary.cmake
@@ -1,4 +1,11 @@
 #
+# Copyright (c) 2014-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+#
 # Requirements:
 # Please provide the following two variables before using these macros:
 #   ${THRIFT1} - path/to/bin/thrift1
