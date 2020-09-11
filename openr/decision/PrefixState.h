d7292ecb705fade180a3284b877b3c625d0cb7c6
Jingyi Yang <girasoley@fb.com>
2020-09-10 20:52:54 -0700

    retire fboss11 - relax nh requirement for bgp routes
    Summary:
    Remove loopback check in Decision for bgp routes.
    - Open/R will program the route with correct nexthops.
    - In bgp speaker, fill nexthop with ::1 and 127.0.0.1 for routes announcing to bgp over ::1.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D22926368
    
    fbshipit-source-id: b87bff9d3970fc2c29eee66b670e7fc1c99ed274

--- openr/decision/PrefixState.h
+++ openr/decision/PrefixState.h
@@ -27,10 +27,6 @@ class PrefixState {
     return prefixes_;
   }
 
-  // update loopback prefix deletes
-  void deleteLoopbackPrefix(
-      thrift::IpPrefix const& prefix, const std::string& nodename);
-
   // returns set of changed prefixes (i.e. a node started advertising or
   // withdrew or any attributes changed)
   std::unordered_set<thrift::IpPrefix> updatePrefixDatabase(
@@ -38,19 +34,6 @@ class PrefixState {
 
   std::unordered_map<std::string /* nodeName */, thrift::PrefixDatabase>
   getPrefixDatabases() const;
-
-  std::vector<thrift::NextHopThrift> getLoopbackVias(
-      std::unordered_set<std::string> const& nodes, bool const isV4) const;
-
-  std::unordered_map<std::string, thrift::BinaryAddress> const&
-  getNodeHostLoopbacksV4() const {
-    return nodeHostLoopbacksV4_;
-  }
-
-  std::unordered_map<std::string, thrift::BinaryAddress> const&
-  getNodeHostLoopbacksV6() const {
-    return nodeHostLoopbacksV6_;
-  }
 
   std::vector<thrift::ReceivedRouteDetail> getReceivedRoutesFiltered(
       thrift::ReceivedRouteFilter const& filter) const;
@@ -84,8 +67,6 @@ class PrefixState {
   std::unordered_map<thrift::IpPrefix, PrefixEntries> prefixes_;
   std::unordered_map<NodeAndArea, std::set<thrift::IpPrefix>> nodeToPrefixes_;
 
-  std::unordered_map<std::string, thrift::BinaryAddress> nodeHostLoopbacksV4_;
-  std::unordered_map<std::string, thrift::BinaryAddress> nodeHostLoopbacksV6_;
 }; // class PrefixState
 
 } // namespace openr
