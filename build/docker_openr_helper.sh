61d1c7a5acef2f5e987191156c97b9e373ff7371
Saif Hasan <has@fb.com>
2020-10-08 17:28:46 -0700

    (easy) Add missing license information to open-sourced files
    Summary: As titled.
    
    Reviewed By: cooperlees, yi-xian, xiangxu1121
    
    Differential Revision: D24205499
    
    fbshipit-source-id: 593086235816de9176b3777163021953781c73a1

--- build/docker_openr_helper.sh
+++ build/docker_openr_helper.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Copyright (c) 2014-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 CFG_BASE="/config"
 DEFAULT_CFG="/etc/openr.conf"
 FB_BASE="/opt/facebook"
