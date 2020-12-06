5ad49034931e1f2199ec2cef69cffd73ee67cd1e
John Strizich <striz@fb.com>
2020-12-05 20:00:16 -0800

    (easy) rm noisy log line
    Summary: added for debugging, should not have landed
    
    Reviewed By: GirasoleY
    
    Differential Revision: D25342388
    
    fbshipit-source-id: 06f9e9ad9ae6b2362e0abe2dbba9244e98e04f27

--- openr/kvstore/KvStorePublisher.cpp
+++ openr/kvstore/KvStorePublisher.cpp
@@ -55,9 +55,7 @@ KvStorePublisher::KvStorePublisher(
  */
 void
 KvStorePublisher::publish(const thrift::Publication& pub) {
-  LOG(INFO) << pub.get_area();
   if (not(selectAreas_.empty() || selectAreas_.count(pub.get_area()))) {
-    LOG(INFO) << "Skipping pub not in selectAreas";
     return;
   }
   if ((not filter_.keys_ref().has_value() or (*filter_.keys_ref()).empty()) and
