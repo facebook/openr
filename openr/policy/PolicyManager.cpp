ea0e72e1cd01ae99ee880fc7131449782b311d19
Jingyi Yang <girasoley@fb.com>
2021-03-17 10:20:08 -0700

    add open source policy implementation
    Summary:
    The change introduce open source policy library `openr/policy`
    ```
    openr/policy/
    ├── PolicyManager.cpp
    ├── PolicyManager.h
    └── TARGETS
    ```
    
    - Define applyPolicy() API in PolicyManager.h
    - Open source (`openr/policy/PolicyManager.cpp`): applyPolicy() behave as always allow.
    - FB implementation (`openr/prefix-manager/facebook/PolicyManager.cpp`) - real policy processing.
    
    What's next:
    - Change Prefix Manager internal data structure to host pre-policy prefix entries and post policy prefix entries per area.
    - Integrate policy manager in PrefixManager.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D26699418
    
    fbshipit-source-id: e6c5687d4710cb86bcca0d69e5970dd55068a910

---
+++ openr/policy/PolicyManager.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "openr/policy/PolicyManager.h"
+
+namespace openr {
+
+class PolicyManagerImpl {};
+
+PolicyManager::PolicyManager(
+    const neteng::config::routing_policy::PolicyConfig& config)
+    : impl_(std::make_unique<PolicyManagerImpl>()) {}
+PolicyManager::~PolicyManager() = default;
+
+std::pair<std::shared_ptr<thrift::PrefixEntry>, std::string /*policy name*/>
+PolicyManager::applyPolicy(
+    const std::string& policyStatementName,
+    const std::shared_ptr<thrift::PrefixEntry>& prefixEntry) noexcept {
+  return {prefixEntry, "Always Allow"};
+}
+
+} // namespace openr
---
+++ openr/policy/PolicyManager.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <configerator/structs/neteng/config/gen-cpp2/routing_policy_types.h>
+#include <openr/if/gen-cpp2/Types_types.h>
+
+namespace openr {
+// Forward declaration
+class PolicyManagerImpl;
+
+/**
+ * PolicyManager manages all policies defined in the config file.
+ */
+class PolicyManager {
+ public:
+  explicit PolicyManager(
+      const neteng::config::routing_policy::PolicyConfig& config);
+  ~PolicyManager();
+
+  std::pair<std::shared_ptr<thrift::PrefixEntry>, std::string /*policy name*/>
+  applyPolicy(
+      const std::string& policyStatementName,
+      const std::shared_ptr<thrift::PrefixEntry>& prefixEntry) noexcept;
+
+  std::unique_ptr<PolicyManagerImpl> impl_{nullptr};
+};
+} // namespace openr
