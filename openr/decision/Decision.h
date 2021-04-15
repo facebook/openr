e13dc1232d3eeff5ab5fce87e9ff365b679bb79b
Michael Liu <michaeluw@fb.com>
2021-04-15 14:36:27 -0700

    Remove Pimpl design for SpfSolver
    Summary:
    Remove Pimpl design for SpfSolver.
    Remove SpfSolverImpl class, all its member will become SpfSolver's private members.
    
    Next diff will move SpfSolver into a separate package.
    
    Reviewed By: xiangxu1121
    
    Differential Revision: D27615646
    
    fbshipit-source-id: f64804053f645975e988c736f741c6ba864902a9

--- openr/decision/Decision.h
+++ openr/decision/Decision.h
@@ -212,8 +212,7 @@ class SpfSolver {
   ~SpfSolver();
 
   //
-  // The following methods talk to implementation so need to
-  // be defined in the .cpp
+  // util function to update IP/MPLS static route
   //
 
   void updateStaticUnicastRoutes(
@@ -239,16 +238,145 @@ class SpfSolver {
       folly::CIDRNetwork const& prefix);
 
   std::unordered_map<folly::CIDRNetwork, BestRouteSelectionResult> const&
-  getBestRoutesCache() const;
+  getBestRoutesCache() const {
+    return bestRoutesCache_;
+  }
 
  private:
   // no-copy
   SpfSolver(SpfSolver const&) = delete;
   SpfSolver& operator=(SpfSolver const&) = delete;
 
-  // pointer to implementation class
-  class SpfSolverImpl;
-  std::unique_ptr<SpfSolverImpl> impl_;
+  std::optional<RibUnicastEntry> createRouteForPrefix(
+      const std::string& myNodeName,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates,
+      PrefixState const& prefixState,
+      folly::CIDRNetwork const& prefix);
+
+  static std::pair<openr::LinkStateMetric, std::unordered_set<std::string>>
+  getMinCostNodes(
+      const LinkState::SpfResult& spfResult,
+      const std::set<NodeAndArea>& dstNodeAreas);
+
+  // Given prefixes and the nodes who announce it, get the ecmp routes.
+  std::optional<RibUnicastEntry> selectBestPathsSpf(
+      std::string const& myNodeName,
+      folly::CIDRNetwork const& prefix,
+      BestRouteSelectionResult const& bestRouteSelectionResult,
+      PrefixEntries const& prefixEntries,
+      bool const isBgp,
+      thrift::PrefixForwardingType const& forwardingType,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates,
+      PrefixState const& prefixState);
+
+  // Given prefixes and the nodes who announce it, get the kspf routes.
+  std::optional<RibUnicastEntry> selectBestPathsKsp2(
+      const std::string& myNodeName,
+      const folly::CIDRNetwork& prefix,
+      BestRouteSelectionResult const& bestRouteSelectionResult,
+      PrefixEntries const& prefixEntries,
+      bool isBgp,
+      thrift::PrefixForwardingType const& forwardingType,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates,
+      PrefixState const& prefixState);
+
+  std::optional<RibUnicastEntry> addBestPaths(
+      const std::string& myNodeName,
+      const folly::CIDRNetwork& prefixThrift,
+      const BestRouteSelectionResult& bestRouteSelectionResult,
+      const PrefixEntries& prefixEntries,
+      const PrefixState& prefixState,
+      const bool isBgp,
+      std::unordered_set<thrift::NextHopThrift>&& nextHops);
+
+  // helper function to find the nodes for the nexthop for bgp route
+  BestRouteSelectionResult runBestPathSelectionBgp(
+      std::string const& myNodeName,
+      folly::CIDRNetwork const& prefix,
+      PrefixEntries const& prefixEntries,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates);
+
+  /**
+   * Performs best route selection from received route announcements
+   */
+  BestRouteSelectionResult selectBestRoutes(
+      std::string const& myNodeName,
+      folly::CIDRNetwork const& prefix,
+      PrefixEntries const& prefixEntries,
+      bool const hasBgp,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates);
+
+  // helper to get min nexthop for a prefix, used in selectKsp2
+  std::optional<int64_t> getMinNextHopThreshold(
+      BestRouteSelectionResult nodes, PrefixEntries const& prefixEntries);
+
+  // Helper to filter overloaded nodes for anycast addresses
+  //
+  // TODO: This should go away, once Open/R policy is in place. The overloaded
+  // nodes will stop advertising specific prefixes if they're overloaded
+  BestRouteSelectionResult maybeFilterDrainedNodes(
+      BestRouteSelectionResult&& result,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates) const;
+
+  // Give source node-name and dstNodeNames, this function returns the set of
+  // nexthops towards these set of dstNodeNames
+  std::pair<
+      openr::LinkStateMetric /* minimum metric to destination */,
+      std::unordered_map<
+          std::pair<std::string /* nextHopNodeName */, std::string /* dest */>,
+          openr::
+              LinkStateMetric /* the distance from the nexthop to the dest */>>
+  getNextHopsWithMetric(
+      const std::string& srcNodeName,
+      const std::set<NodeAndArea>& dstNodeAreas,
+      bool perDestination,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates);
+
+  // This function converts best nexthop nodes to best nexthop adjacencies
+  // which can then be passed to FIB for programming. It considers and
+  // parallel link logic (tested by our UT)
+  // If swap label is provided then it will be used to associate SWAP or PHP
+  // mpls action
+  std::unordered_set<thrift::NextHopThrift> getNextHopsThrift(
+      const std::string& myNodeName,
+      const std::set<NodeAndArea>& dstNodeAreas,
+      bool isV4,
+      bool v4OverV6Nexthop,
+      bool perDestination,
+      const openr::LinkStateMetric minMetric,
+      std::unordered_map<
+          std::pair<std::string, std::string>,
+          openr::LinkStateMetric> nextHopNodes,
+      std::optional<int32_t> swapLabel,
+      std::unordered_map<std::string, LinkState> const& areaLinkStates,
+      PrefixEntries const& prefixEntries = {}) const;
+
+  // Collection to store static IP/MPLS routes
+  StaticMplsRoutes staticMplsRoutes_;
+  StaticUnicastRoutes staticUnicastRoutes_;
+
+  // Cache of best route selection.
+  // - Cleared when topology changes
+  // - Updated for the prefix whenever a route is created for it
+  std::unordered_map<folly::CIDRNetwork, BestRouteSelectionResult>
+      bestRoutesCache_;
+
+  const std::string myNodeName_;
+
+  // is v4 enabled. If yes then Decision will forward v4 prefixes with v4
+  // nexthops to Fib module for programming. Else it will just drop them.
+  const bool enableV4_{false};
+
+  const bool enableAdjacencyLabels_{true};
+
+  const bool bgpDryRun_{false};
+
+  const bool enableBestRouteSelection_{false};
+
+  // is v4 over v6 nexthop enabled. If yes then Decision will forward v4
+  // prefixes with v6 nexthops to Fib module for programming. Else it will just
+  // use v4 over v4 nexthop.
+  const bool v4OverV6Nexthop_{false};
 };
 
 //
