923c5fc03b5b2a61d2ff6e22485347b64b2745f2
Saif Hasan <has@fb.com>
2021-07-02 09:23:13 -0700

    Improve benchmark of ReplicateQueue
    Summary:
    Major changes
    - Use primitive data type for load testing. This will avoid data copy overhead
      of message and would help us highlight the performance of messaging library
    - Create readers & writers in separate thread
    - Create `kNumReaders` instead of `iters * kNumReaders` previously. Readers are
      always hot, and trying to read from the queue
    - For each iteration we create set of writer tasks
    
    The cost of 1 million message exchange between 1 reader writer is 356ms. With
    10 readers it is approximately 400ms per reader, 100 readers it is 261ms per reader.
    
    Changing number of writers doesn't affect throughput
    
    Reviewed By: xiangxu1121
    
    Differential Revision: D29528767
    
    fbshipit-source-id: 98934a90dc071d418cb345c1cde45866a84c5479

---
+++ openr/messaging/tests/MessagingBenchmark.cpp
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <folly/Benchmark.h>
+#include <folly/fibers/FiberManagerMap.h>
+#include <folly/init/Init.h>
+#include <folly/io/async/EventBase.h>
+
+#include <openr/messaging/ReplicateQueue.h>
+
+namespace openr {
+
+static void
+BM_ReplicateQueue(
+    uint32_t iters,
+    const size_t kNumReaders,
+    const size_t kNumWriters,
+    const size_t kCount) {
+  auto suspender = folly::BenchmarkSuspender();
+
+  //
+  // Total number of reads performed
+  //
+  std::atomic<size_t> totalReads{0};
+
+  //
+  // Queue under testing. We use primitive type. This is good enough for us to
+  // measure the performance overhead of messaging queue.
+  //
+  messaging::ReplicateQueue<size_t> q;
+
+  //
+  // Add reader tasks. Reader would continue to read as long as queue is open
+  // NOTE: We have all readers in their own event base & thread
+  //
+  folly::EventBase readerEvb;
+  auto& readerManager = folly::fibers::getFiberManager(readerEvb);
+  for (size_t i = 0; i < kNumReaders; ++i) {
+    readerManager.addTask([reader = q.getReader(), i, &totalReads]() mutable {
+      size_t numReads{0};
+      while (true) {
+        auto maybeNum = reader.get();
+        if (maybeNum.hasError()) {
+          break; // Queue is closed
+        }
+        ++numReads;
+        ++totalReads;
+      }
+      VLOG(1) << "Reader-" << i << " consumed " << numReads << " messages";
+    });
+  }
+
+  //
+  // Start reader thread. This will not return until all reader tasks are
+  // completed.
+  //
+  std::thread readerThread([&readerEvb] { readerEvb.loop(); });
+
+  //
+  // Iterate multiple times. In each iterate we
+  //
+  while (iters--) {
+    //
+    // Add writer tasks. Each writer will write `kCount` elements. So overall
+    // each reader would read `kCount * kNumWriters` elements. Aka each reader
+    // would read every element written by every reader.
+    //
+    folly::EventBase writerEvb;
+    auto& writerManager = folly::fibers::getFiberManager(writerEvb);
+    for (size_t i = 0; i < kNumWriters; ++i) {
+      writerManager.addTask([&q, kCount, i]() {
+        for (size_t m = 0; m < kCount; ++m) {
+          q.push(m);
+        }
+        VLOG(1) << "Writer-" << i << " finished writing " << kCount
+                << " messages.";
+      });
+    }
+
+    //
+    // Run writer-loop & wait until reader reads everything
+    //
+    const size_t expectedReads = kCount * kNumWriters * kNumReaders;
+    totalReads = 0;
+    suspender.dismiss();
+    writerEvb.loop(); // Publish all writes
+    while (totalReads != expectedReads) {
+      std::this_thread::yield();
+    }
+    suspender.rehire();
+  } // while
+
+  //
+  // Close queue & wait for all readers to terminate
+  //
+  q.close();
+  readerThread.join();
+}
+
+/**
+ * The first parameter is number of readers
+ * The second parameter is the number of writers
+ * The third parameter is the number of messages each writer sends
+ *
+ * In our benchmark we intends to keep Number of Messages Written same. So when
+ * we increase writers, we reduce number of messages per writer (third param)
+ */
+
+BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R1_W1, 1, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R10_W1, 10, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R100_W1, 100, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R1_W10, 1, 10, 100000);
+BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R1_W100, 1, 100, 10000);
+
+} // namespace openr
+
+int
+main(int argc, char** argv) {
+  folly::init(&argc, &argv);
+  folly::runBenchmarks();
+  return 0;
+}
