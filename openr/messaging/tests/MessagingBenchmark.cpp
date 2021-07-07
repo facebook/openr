f792df2b0b639798d979ef6bd51381803684ed7d
Saif Hasan <has@fb.com>
2021-07-07 16:27:41 -0700

    Add benchmark for RWQueue
    Summary:
    RWQueue benchmark is slightly different from ReplicateQueue. Here the writes are
    distributed across all the readers.
    This benchmarks helps us understand throughput of RWQueue in various conditions
    
    Conclusion
    - Takes 250ms for 1 million messages
    - Increasing #readers doesn't introduces too much overhead.
    - Increasing #writers doesn't introduces overhead
    
    Reviewed By: xiangxu1121
    
    Differential Revision: D29531367
    
    fbshipit-source-id: 7212f7df4489bda38a11dee7866292ea6ee3e892

--- openr/messaging/tests/MessagingBenchmark.cpp
+++ openr/messaging/tests/MessagingBenchmark.cpp
@@ -10,9 +10,97 @@
 #include <folly/init/Init.h>
 #include <folly/io/async/EventBase.h>
 
+#include <openr/messaging/Queue.h>
 #include <openr/messaging/ReplicateQueue.h>
 
 namespace openr {
+
+static void
+BM_RWQueue(
+    uint32_t iters,
+    const size_t kNumReaders,
+    const size_t kNumWriters,
+    const size_t kCount) {
+  auto suspender = folly::BenchmarkSuspender();
+
+  //
+  // Total number of reads performed
+  //
+  std::atomic<size_t> totalReads{0};
+
+  //
+  // Queue under testing. We use primitive type. This is good enough for us to
+  // measure the performance overhead of messaging queue.
+  //
+  messaging::RWQueue<size_t> q;
+
+  //
+  // Add reader tasks. Reader would continue to read as long as queue is open
+  // NOTE: We have all readers in their own event base & thread
+  //
+  folly::EventBase readerEvb;
+  auto& readerManager = folly::fibers::getFiberManager(readerEvb);
+  for (size_t i = 0; i < kNumReaders; ++i) {
+    readerManager.addTask([&q, i, &totalReads]() mutable {
+      size_t numReads{0};
+      while (true) {
+        auto maybeNum = q.get();
+        if (maybeNum.hasError()) {
+          break; // Queue is closed
+        }
+        ++numReads;
+        ++totalReads;
+      }
+      VLOG(1) << "Reader-" << i << " consumed " << numReads << " messages";
+    });
+  }
+
+  //
+  // Start reader thread. This will not return until all reader tasks are
+  // completed.
+  //
+  std::thread readerThread([&readerEvb] { readerEvb.loop(); });
+
+  //
+  // Iterate multiple times. In each iterate we
+  //
+  while (iters--) {
+    //
+    // Add writer tasks. Each writer will write `kCount` elements. So overall
+    // each reader would read `kCount * kNumWriters` elements. Aka each reader
+    // would read every element written by every reader.
+    //
+    folly::EventBase writerEvb;
+    auto& writerManager = folly::fibers::getFiberManager(writerEvb);
+    for (size_t i = 0; i < kNumWriters; ++i) {
+      writerManager.addTask([&q, kCount, i]() {
+        for (size_t m = 0; m < kCount; ++m) {
+          q.push(m);
+        }
+        VLOG(1) << "Writer-" << i << " finished writing " << kCount
+                << " messages.";
+      });
+    }
+
+    //
+    // Run writer-loop & wait until reader reads everything
+    //
+    const size_t expectedReads = kCount * kNumWriters;
+    totalReads = 0;
+    suspender.dismiss();
+    writerEvb.loop(); // Publish all writes
+    while (totalReads != expectedReads) {
+      std::this_thread::yield();
+    }
+    suspender.rehire();
+  } // while
+
+  //
+  // Close queue & wait for all readers to terminate
+  //
+  q.close();
+  readerThread.join();
+}
 
 static void
 BM_ReplicateQueue(
@@ -109,6 +197,14 @@ BM_ReplicateQueue(
  * In our benchmark we intends to keep Number of Messages Written same. So when
  * we increase writers, we reduce number of messages per writer (third param)
  */
+
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R1_W1, 1, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R10_W1, 10, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R100_W1, 100, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R1000_W1, 1000, 1, 1000000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R1_W10, 1, 10, 100000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R1_W100, 1, 100, 10000);
+BENCHMARK_NAMED_PARAM(BM_RWQueue, M1000000_R1_W1000, 1, 1000, 1000);
 
 BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R1_W1, 1, 1, 1000000);
 BENCHMARK_NAMED_PARAM(BM_ReplicateQueue, M1000000_R10_W1, 10, 1, 1000000);
