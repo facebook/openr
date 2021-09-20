00bf686ddfcd90eb3b7d0ce1feecec942831f184
Xiang Xu <xiangxu1121@fb.com>
2021-09-20 11:00:05 -0700

    Fix OpenrCtrlLongPollTest under OSS with getdeps.py
    Summary: As titled. Migrate to use `OpenrCtrlHandler` instead of using thrift client to mimick API behavior.
    
    Reviewed By: yi-xian
    
    Differential Revision: D31044266
    
    fbshipit-source-id: 19e2461a9be06cad8aebe2e566649b6c2b861052

--- openr/ctrl-server/tests/OpenrCtrlLongPollTest.cpp
+++ openr/ctrl-server/tests/OpenrCtrlLongPollTest.cpp
@@ -30,28 +30,14 @@ class LongPollFixture : public ::testing::Test {
     kvStoreWrapper_ = std::make_unique<KvStoreWrapper>(context_, config_);
     kvStoreWrapper_->run();
 
-    // initialize openrCtrlClient talking to server
-    client1_ = getOpenrCtrlPlainTextClient<apache::thrift::HeaderClientChannel>(
-        evb_,
-        folly::IPAddress(Constants::kPlatformHost.toString()),
-        kvStoreWrapper_->getThriftPort());
-
-    // Create client to have client-side timeout longer than OpenrCtrlServer
-    // side default. This mimick we are NOT timeout but receive "false"
-    // indicating no changes.
-    client2_ = getOpenrCtrlPlainTextClient<apache::thrift::HeaderClientChannel>(
-        evb_,
-        folly::IPAddress(Constants::kPlatformHost.toString()),
-        kvStoreWrapper_->getThriftPort(),
-        Constants::kServiceConnTimeout,
-        Constants::kLongPollReqHoldTime + std::chrono::milliseconds(10000));
+    // initialize OpenrCtrlHandler for testing usage
+    handler_ = kvStoreWrapper_->getThriftServerCtrlHandler();
   }
 
   void
   TearDown() override {
     kvStoreWrapper_->closeQueue();
-    client1_.reset();
-    client2_.reset();
+    handler_.reset();
     kvStoreWrapper_->stop();
     kvStoreWrapper_.reset();
   }
@@ -68,118 +54,104 @@ class LongPollFixture : public ::testing::Test {
   openr::OpenrEventBase testEvb_;
   std::shared_ptr<Config> config_{nullptr};
   std::unique_ptr<KvStoreWrapper> kvStoreWrapper_;
-  std::unique_ptr<openr::thrift::OpenrCtrlCppAsyncClient> client1_{nullptr};
-  std::unique_ptr<openr::thrift::OpenrCtrlCppAsyncClient> client2_{nullptr};
+  std::shared_ptr<OpenrCtrlHandler> handler_{nullptr};
 };
 
+/*
+ * This UT mimicks the basic functionality of long poll API to make sure
+ * server will return to client if there is "adj:" key change received.
+ */
 TEST_F(LongPollFixture, LongPollAdjAdded) {
-  //
-  // This UT mimicks the basic functionality of long poll API to make sure
-  // server will return to client if there is "adj:" key change received.
-  //
   bool isAdjChanged = false;
-  bool isTimeout = false;
   std::chrono::steady_clock::time_point startTime;
   std::chrono::steady_clock::time_point endTime;
 
   // mimick there is a new publication from kvstore
   testEvb_.scheduleTimeout(std::chrono::milliseconds(1000), [&]() noexcept {
-    LOG(INFO) << "AdjKey set...";
-    // catch  up the time
+    // record the time
     startTime = std::chrono::steady_clock::now();
     kvStoreWrapper_->setKey(
         kTestingAreaName,
         adjKey_,
         createThriftValue(1, nodeName_, std::string("value1")));
 
-    // stop the evl
+    // stop the evb
     testEvb_.stop();
   });
 
   // start eventloop
-  std::thread evlThread([&]() { testEvb_.run(); });
+  std::thread evbThread([&]() { testEvb_.run(); });
   testEvb_.waitUntilRunning();
 
-  // client starts to do long-poll
-  try {
-    // By default, the processing timeout value for client is 10s.
-    LOG(INFO) << "Start long poll...";
-    thrift::KeyVals snapshot;
-    isAdjChanged =
-        client1_->sync_longPollKvStoreAdjArea(kTestingAreaName, snapshot);
-    endTime = std::chrono::steady_clock::now();
-    LOG(INFO) << "Finished long poll...";
-  } catch (std::exception& ex) {
-    LOG(INFO) << "Exception happened: " << folly::exceptionStr(ex);
-    isTimeout = true;
-  }
+  // start long-poll
+  LOG(INFO) << "Start long poll...";
+  isAdjChanged = handler_
+                     ->semifuture_longPollKvStoreAdjArea(
+                         std::make_unique<std::string>(kTestingAreaName),
+                         std::make_unique<thrift::KeyVals>())
+                     .get();
+  endTime = std::chrono::steady_clock::now();
+  LOG(INFO) << "Finished long poll...";
 
-  // make sure we are receiving update and NOT timed out
-  ASSERT_FALSE(isTimeout);
-  // make sure when there is publication, processing delay is less than 450ms
-  ASSERT_LE(endTime - startTime, std::chrono::milliseconds(450));
+  // make sure when there is publication, processing delay is less than 250ms
+  ASSERT_LE(endTime - startTime, std::chrono::milliseconds(250));
   ASSERT_TRUE(isAdjChanged);
 
   // wait for evl before cleanup
   testEvb_.waitUntilStopped();
-  evlThread.join();
+  evbThread.join();
 }
 
+/*
+ * This UT mimicks the scenario there is a client side timeout since
+ * there is NO "adj:" key change.
+ */
 TEST_F(LongPollFixture, LongPollTimeout) {
-  //
-  // This UT mimicks the scenario there is a client side timeout since
-  // there is NO "adj:" key change.
-  //
-  bool isTimeout = false;
   bool isAdjChanged = false;
 
   // mimick there is a new publication from kvstore
-  testEvb_.scheduleTimeout(std::chrono::milliseconds(5000), [&]() noexcept {
-    LOG(INFO) << "Prefix key set...";
-    kvStoreWrapper_->setKey(
-        kTestingAreaName,
-        prefixKey_,
-        createThriftValue(1, nodeName_, std::string("value1")));
+  testEvb_.scheduleTimeout(
+      Constants::kLongPollReqHoldTime + std::chrono::milliseconds(1000),
+      [&]() noexcept {
+        kvStoreWrapper_->setKey(
+            kTestingAreaName,
+            prefixKey_,
+            createThriftValue(1, nodeName_, std::string("value1")));
 
-    // stop the evl
-    testEvb_.stop();
-  });
+        // stop the evl
+        testEvb_.stop();
+      });
 
   // start eventloop
-  std::thread evlThread([&]() { testEvb_.run(); });
+  std::thread evbThread([&]() { testEvb_.run(); });
   testEvb_.waitUntilRunning();
 
-  // client starts to do long-poll
-  try {
-    // By default, the processing timeout value for client is 10s.
-    LOG(INFO) << "Start long poll...";
-    thrift::KeyVals snapshot;
-    isAdjChanged =
-        client1_->sync_longPollKvStoreAdjArea(kTestingAreaName, snapshot);
-  } catch (std::exception& ex) {
-    LOG(INFO) << "Exception happened: " << folly::exceptionStr(ex);
-    isTimeout = true;
-  }
+  // start long-poll
+  LOG(INFO) << "Start long poll...";
+  isAdjChanged = handler_
+                     ->semifuture_longPollKvStoreAdjArea(
+                         std::make_unique<std::string>(kTestingAreaName),
+                         std::make_unique<thrift::KeyVals>())
+                     .get();
+  LOG(INFO) << "Finished long poll...";
 
-  // Client timeout and nothing received
-  ASSERT_TRUE(isTimeout);
+  // Server side timeout with no adj change
   ASSERT_FALSE(isAdjChanged);
 
   // Explicitly cleanup pending longPollReq
-  kvStoreWrapper_->getThriftServerCtrlHandler()->cleanupPendingLongPollReqs();
+  handler_->cleanupPendingLongPollReqs();
 
   // wait for evl before cleanup
   testEvb_.waitUntilStopped();
-  evlThread.join();
+  evbThread.join();
 }
 
+/*
+ * This UT mimicks the scenario that before client send req.
+ * There is already "adj:" key published before client subscribe.
+ * Should push immediately.
+ */
 TEST_F(LongPollFixture, LongPollAdjModified) {
-  //
-  // This UT mimicks the scenario that before client send req.
-  // There is already "adj:" key published before client subscribe.
-  // Should push immediately.
-  //
-  bool isTimeout = false;
   bool isAdjChanged = false;
   std::chrono::steady_clock::time_point startTime;
   std::chrono::steady_clock::time_point endTime;
@@ -190,36 +162,32 @@ TEST_F(LongPollFixture, LongPollAdjModified) {
       adjKey_,
       createThriftValue(2, nodeName_, std::string("value1")));
 
-  try {
-    // mimicking scenario that server has different value for the same key
-    thrift::KeyVals snapshot;
-    snapshot.emplace(
-        adjKey_, createThriftValue(1, "Valar-Dohaeris", std::string("value1")));
+  // mimicking scenario that server has different value for the same key
+  thrift::KeyVals snapshot;
+  snapshot.emplace(
+      adjKey_, createThriftValue(1, "Valar-Dohaeris", std::string("value1")));
 
-    // By default, the processing timeout value for client is 10s.
-    LOG(INFO) << "Start long poll...";
-    startTime = std::chrono::steady_clock::now();
-    isAdjChanged =
-        client1_->sync_longPollKvStoreAdjArea(kTestingAreaName, snapshot);
-    endTime = std::chrono::steady_clock::now();
-    LOG(INFO) << "Finished long poll...";
-  } catch (std::exception& ex) {
-    LOG(INFO) << "Exception happened: " << folly::exceptionStr(ex);
-    isTimeout = true;
-  }
+  // By default, the processing timeout value for client is 10s.
+  LOG(INFO) << "Start long poll...";
+  startTime = std::chrono::steady_clock::now();
+  isAdjChanged = handler_
+                     ->semifuture_longPollKvStoreAdjArea(
+                         std::make_unique<std::string>(kTestingAreaName),
+                         std::make_unique<thrift::KeyVals>(std::move(snapshot)))
+                     .get();
+  endTime = std::chrono::steady_clock::now();
+  LOG(INFO) << "Finished long poll...";
 
-  ASSERT_FALSE(isTimeout);
   // make sure when there is publication, processing delay is less than 50ms
   ASSERT_LE(endTime - startTime, std::chrono::milliseconds(50));
   ASSERT_TRUE(isAdjChanged);
 }
 
+/*
+ * This UT mimicks the scenario that client already hold the same adj key.
+ * Server will NOT push notification since there is no delta generated.
+ */
 TEST_F(LongPollFixture, LongPollAdjUnchanged) {
-  //
-  // This UT mimicks the scenario that client already hold the same adj key.
-  // Server will NOT push notification since there is no diff.
-  //
-  bool isTimeout = false;
   bool isAdjChanged = true;
 
   // inject key to kvstore and openrCtrlThriftServer should have adj key
@@ -231,9 +199,8 @@ TEST_F(LongPollFixture, LongPollAdjUnchanged) {
   // mimick there is a new publication from kvstore.
   // This publication should clean up pending req.
   testEvb_.scheduleTimeout(
-      Constants::kLongPollReqHoldTime + std::chrono::milliseconds(5000),
+      Constants::kLongPollReqHoldTime + std::chrono::milliseconds(1000),
       [&]() noexcept {
-        LOG(INFO) << "Prefix key set...";
         kvStoreWrapper_->setKey(
             kTestingAreaName,
             prefixKey_,
@@ -244,61 +211,53 @@ TEST_F(LongPollFixture, LongPollAdjUnchanged) {
       });
 
   // start eventloop
-  std::thread evlThread([&]() { testEvb_.run(); });
+  std::thread evbThread([&]() { testEvb_.run(); });
   testEvb_.waitUntilRunning();
 
-  try {
-    thrift::KeyVals snapshot;
-    snapshot.emplace(
-        adjKey_, createThriftValue(1, nodeName_, std::string("value1")));
+  thrift::KeyVals snapshot;
+  snapshot.emplace(
+      adjKey_, createThriftValue(1, nodeName_, std::string("value1")));
 
-    LOG(INFO) << "Start long poll...";
-    isAdjChanged =
-        client2_->sync_longPollKvStoreAdjArea(kTestingAreaName, snapshot);
-    LOG(INFO) << "Finished long poll...";
-  } catch (std::exception& ex) {
-    LOG(INFO) << "Exception happened: " << folly::exceptionStr(ex);
-    isTimeout = true;
-  }
+  LOG(INFO) << "Start long poll...";
+  isAdjChanged = handler_
+                     ->semifuture_longPollKvStoreAdjArea(
+                         std::make_unique<std::string>(kTestingAreaName),
+                         std::make_unique<thrift::KeyVals>(std::move(snapshot)))
+                     .get();
+  LOG(INFO) << "Finished long poll...";
 
   ASSERT_FALSE(isAdjChanged);
-  ASSERT_FALSE(isTimeout);
 
   // wait for evl before cleanup
   testEvb_.waitUntilStopped();
-  evlThread.join();
+  evbThread.join();
 }
 
+/*
+ * This UT mimicks the scenario that client hold adj key,
+ * but server doesn't have this adj key(e.g. adj key expiration)
+ */
 TEST_F(LongPollFixture, LongPollAdjExpired) {
-  //
-  // This UT mimicks the scenario that client hold adj key,
-  // but server doesn't have this adj key( e.g. adj key expiration )
-  //
-  bool isTimeout = false;
   bool isAdjChanged = false;
 
   std::chrono::steady_clock::time_point startTime;
   std::chrono::steady_clock::time_point endTime;
 
-  try {
-    // mimicking scenario that server has different value for the same key
-    thrift::KeyVals snapshot;
-    snapshot.emplace(
-        adjKey_, createThriftValue(1, "Valar-Dohaeris", std::string("value1")));
+  // mimicking scenario that server has different value for the same key
+  thrift::KeyVals snapshot;
+  snapshot.emplace(
+      adjKey_, createThriftValue(1, "Valar-Dohaeris", std::string("value1")));
 
-    // By default, the processing timeout value for client is 10s.
-    LOG(INFO) << "Start long poll...";
-    startTime = std::chrono::steady_clock::now();
-    isAdjChanged =
-        client1_->sync_longPollKvStoreAdjArea(kTestingAreaName, snapshot);
-    endTime = std::chrono::steady_clock::now();
-    LOG(INFO) << "Finished long poll...";
-  } catch (std::exception& ex) {
-    LOG(INFO) << "Exception happened: " << folly::exceptionStr(ex);
-    isTimeout = true;
-  }
+  LOG(INFO) << "Start long poll...";
+  startTime = std::chrono::steady_clock::now();
+  isAdjChanged = handler_
+                     ->semifuture_longPollKvStoreAdjArea(
+                         std::make_unique<std::string>(kTestingAreaName),
+                         std::make_unique<thrift::KeyVals>(std::move(snapshot)))
+                     .get();
+  endTime = std::chrono::steady_clock::now();
+  LOG(INFO) << "Finished long poll...";
 
-  ASSERT_FALSE(isTimeout);
   // make sure when there is publication, processing delay is less than 50ms
   ASSERT_LE(endTime - startTime, std::chrono::milliseconds(50));
   ASSERT_TRUE(isAdjChanged);
