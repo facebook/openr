794f96183be50f247560e785e1613716d285f78e
Yixian Jiang <yixianj@fb.com>
2022-01-26 16:40:27 -0800

    use realpath() for reading the file path
    Summary:
    Fixes CVE-2021-20439. It prevents directory traversal attacks.
    
     ---
    
    Reviewed By: TangoRoxy, xiangxu1121
    
    Differential Revision: D33801073
    
    fbshipit-source-id: a9d23c15ecc56298cccdc4cf6f0b227443f874a3

--- openr/common/FileUtil.cpp
+++ openr/common/FileUtil.cpp
@@ -6,12 +6,24 @@
  */
 
 #include "openr/common/FileUtil.h"
+#include <folly/logging/xlog.h>
 
 namespace openr {
 
 bool
-FileUtil::readFileToString(const std::string& filePath, std::string& contents) {
-  return folly::readFile(filePath.c_str(), contents);
+FileUtil::readFileToString(const std::string& path, std::string& contents) {
+  // realpath() expands all symbolic links and resolves references to
+  // /./, /../ and extra '/' characters in the null-terminated string
+  // named by path to produce a canonicalized absolute pathname.
+
+  // This prevents directory traversal attacks, e.g. writing to
+  // "../../../etc/passwd".
+  std::array<char, PATH_MAX + 1> resolvedPath;
+  realpath(path.c_str(), resolvedPath.data());
+  XLOG(INFO) << "Read the file: " << resolvedPath.data();
+
+  // read the file
+  return folly::readFile(resolvedPath.data(), contents);
 }
 
 } // namespace openr
