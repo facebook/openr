624e4ee46651dedc70bf3c96d113f0dc51744438
Charitha Bandi <chbandi@fb.com>
2022-06-30 10:26:10 -0700

    Intro to Mock framework
    Summary: Tried to mock lm links command
    
    Reviewed By: cooperlees, xiangxu1121
    
    Differential Revision: D37539176
    
    fbshipit-source-id: 7a1e3da11c4d38287a32e6693f455844fff589f9

--- openr/py/openr/cli/tests/lm/fixtures.py
+++ openr/py/openr/cli/tests/lm/fixtures.py
@@ -5,3 +5,83 @@
 # LICENSE file in the root directory of this source tree.
 
 # pyre-strict
+
+
+from openr.Network.ttypes import BinaryAddress, IpPrefix
+from openr.Types.ttypes import DumpLinksReply, InterfaceDetails, InterfaceInfo
+
+LM_LINKS_OPENR_RIGHT_OK = DumpLinksReply(
+    thisNodeName="openr-right",
+    nodeMetricIncrementVal=0,
+    isOverloaded=False,
+    interfaceDetails={
+        "lo": InterfaceDetails(
+            info=InterfaceInfo(
+                isUp=True,
+                ifIndex=1,
+                networks=[
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(addr=b"\x7f\x00\x00\x01"),
+                        prefixLength=8,
+                    ),
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(addr=b"\n\x06\t\x02"),
+                        prefixLength=32,
+                    ),
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(
+                            addr=b"\xfd\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                        ),
+                        prefixLength=64,
+                    ),
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(
+                            addr=b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01"
+                        ),
+                        prefixLength=128,
+                    ),
+                ],
+            ),
+            isOverloaded=False,
+            linkMetricIncrementVal=0,
+        ),
+        "right0": InterfaceDetails(
+            info=InterfaceInfo(
+                isUp=True,
+                ifIndex=63,
+                networks=[
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(
+                            addr=b"\xfd\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02"
+                        ),
+                        prefixLength=64,
+                    ),
+                    IpPrefix(
+                        prefixAddress=BinaryAddress(
+                            addr=b"\xfe\x80\x00\x00\x00\x00\x00\x00P\xef\xde\xff\xfeL\xd4\xf6"
+                        ),
+                        prefixLength=64,
+                    ),
+                ],
+            ),
+            isOverloaded=False,
+            linkMetricIncrementVal=0,
+        ),
+    },
+)
+
+LM_LINKS_EXPECTED_OPENR_RIGHT_STDOUT = """
+== Node Overload: NO  ==
+
+Interface    Status    Metric Override    Addresses
+-----------  --------  -----------------  -------------------------
+lo           Up
+                                          127.0.0.1
+                                          10.6.9.2
+                                          fd00:2::
+                                          ::1
+right0       Up
+                                          fd00::2
+                                          fe80::50ef:deff:fe4c:d4f6
+
+"""
--- openr/py/openr/cli/tests/lm/tests.py
+++ openr/py/openr/cli/tests/lm/tests.py
@@ -6,12 +6,18 @@
 
 # pyre-strict
 
+from unittest.mock import MagicMock, patch
+
 from click.testing import CliRunner
 from later.unittest import TestCase
 from openr.cli.clis import lm
+from openr.cli.tests import helpers
 
 
 BASE_MODULE = "openr.cli.clis.lm"
+BASE_CMD_MODULE = "openr.cli.commands.lm"
+
+from .fixtures import LM_LINKS_EXPECTED_OPENR_RIGHT_STDOUT, LM_LINKS_OPENR_RIGHT_OK
 
 
 class CliLmTests(TestCase):
@@ -25,3 +31,17 @@ class CliLmTests(TestCase):
             catch_exceptions=False,
         )
         self.assertEqual(0, invoked_return.exit_code)
+
+    @patch(helpers.COMMANDS_GET_OPENR_CTRL_CLIENT)
+    def test_lm_links(self, mocked_openr_client: MagicMock) -> None:
+        mocked_returned_connection = helpers.get_enter_thrift_magicmock(
+            mocked_openr_client
+        )
+        mocked_returned_connection.getInterfaces.return_value = LM_LINKS_OPENR_RIGHT_OK
+        invoked_return = self.runner.invoke(
+            lm.LMLinksCli.links,
+            [],
+            catch_exceptions=False,
+        )
+        self.assertEqual(0, invoked_return.exit_code)
+        self.assertEqual(LM_LINKS_EXPECTED_OPENR_RIGHT_STDOUT, invoked_return.stdout)
