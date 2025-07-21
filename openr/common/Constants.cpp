da3af39825a869098ac8697073774ca4197ccf4a
Richard Barnes <rbarnes@meta.com>
2025-07-21 08:55:51 -0700

    Del redundant-static-def in openr/common/Constants.cpp +1
    Summary:
    LLVM has a warning `-Wdeprecated-redundant-constexpr-static-def` which raises the warning:
    
    > warning: out-of-line definition of constexpr static data member is redundant in C++17 and is deprecated
    
    Since we are now on C++20, we can remove the out-of-line definition of constexpr static data members. This diff does so.
    
     - If you approve of this diff, please use the "Accept & Ship" button :-)
    
    Reviewed By: meyering
    
    Differential Revision: D78635017
    
    fbshipit-source-id: ee387cd4722aa4bdd35dc14e43db45d44b22a5af

--- openr/common/Constants.cpp
+++ openr/common/Constants.cpp
@@ -9,25 +9,6 @@
 
 namespace openr {
 
-constexpr std::chrono::milliseconds Constants::kFibMaxBackoff;
-constexpr std::chrono::milliseconds Constants::kFloodPendingPublication;
-constexpr std::chrono::milliseconds Constants::kInitialBackoff;
-constexpr std::chrono::milliseconds Constants::kKeepAliveCheckInterval;
-constexpr std::chrono::milliseconds Constants::kKvStoreClearThrottleTimeout;
-constexpr std::chrono::milliseconds Constants::kKvStoreSyncThrottleTimeout;
-constexpr std::chrono::milliseconds Constants::kLinkImmediateTimeout;
-constexpr std::chrono::milliseconds Constants::kLinkThrottleTimeout;
-constexpr std::chrono::milliseconds Constants::kLongPollReqHoldTime;
-constexpr std::chrono::milliseconds Constants::kMaxBackoff;
-constexpr std::chrono::milliseconds Constants::kMaxTtlUpdateInterval;
-constexpr std::chrono::milliseconds Constants::kPersistentStoreInitialBackoff;
-constexpr std::chrono::milliseconds Constants::kPersistentStoreMaxBackoff;
-constexpr std::chrono::milliseconds Constants::kPlatformConnTimeout;
-constexpr std::chrono::milliseconds Constants::kPlatformProcTimeout;
-constexpr std::chrono::milliseconds Constants::kReadTimeout;
-constexpr std::chrono::milliseconds Constants::kServiceConnTimeout;
-constexpr std::chrono::milliseconds Constants::kServiceConnSSLTimeout;
-constexpr std::chrono::milliseconds Constants::kServiceProcTimeout;
 constexpr std::chrono::milliseconds Constants::kTtlDecrement;
 constexpr std::chrono::milliseconds Constants::kTtlInfInterval;
 constexpr std::chrono::milliseconds Constants::kTtlThreshold;
