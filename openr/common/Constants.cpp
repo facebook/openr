defb845b6b3e88e9c0ffd790f63b49444558cce2
Richard Barnes <rbarnes@meta.com>
2025-08-08 11:21:36 -0700

    Del redundant-static-def in openr/common/Constants.cpp +1
    Summary:
    LLVM has a warning `-Wdeprecated-redundant-constexpr-static-def` which raises the warning:
    
    > warning: out-of-line definition of constexpr static data member is redundant in C++17 and is deprecated
    
    Since we are now on C++20, we can remove the out-of-line definition of constexpr static data members. This diff does so.
    
     - If you approve of this diff, please use the "Accept & Ship" button :-)
    
    Differential Revision: D79835136
    
    fbshipit-source-id: 61c1710502e88ce444e322d421bae051581f4e1f

--- openr/common/Constants.cpp
+++ openr/common/Constants.cpp
@@ -7,19 +7,4 @@
 
 #include "Constants.h"
 
-namespace openr {
-
-constexpr std::chrono::milliseconds Constants::kTtlDecrement;
-constexpr std::chrono::milliseconds Constants::kTtlInfInterval;
-constexpr std::chrono::milliseconds Constants::kTtlThreshold;
-constexpr std::chrono::seconds Constants::kConvergenceMaxDuration;
-constexpr std::chrono::seconds Constants::kCounterSubmitInterval;
-constexpr std::chrono::seconds Constants::kFloodTopoDumpInterval;
-constexpr std::chrono::seconds Constants::kMemoryThresholdTime;
-constexpr std::chrono::seconds Constants::kPlatformSyncInterval;
-constexpr std::chrono::seconds Constants::kPlatformThriftIdleTimeout;
-constexpr std::chrono::seconds Constants::kThriftClientKeepAliveInterval;
-constexpr uint16_t Constants::kPerfBufferSize;
-constexpr uint32_t Constants::kMaxAllowedPps;
-
-} // namespace openr
+namespace openr {} // namespace openr
