aa619ecc6c1c4a8431599dfa55c69c8937c55d3c
Marcus Loo <loo@meta.com>
2026-01-20 11:41:58 -0800

    remove unused deprecated functions in ipnetwork_deprecated
    Summary: With D90724611, the below deprecated functions are no longer used. So we can remove them.
    
    Differential Revision: D90829037
    
    fbshipit-source-id: 7b0102d9582041a0bebf3d50bbf3dc7bae57cb97

--- openr/py/openr/utils/ipnetwork_deprecated.py
+++ openr/py/openr/utils/ipnetwork_deprecated.py
@@ -10,24 +10,6 @@
 import socket
 
 from openr.Network import ttypes as network_types
-from openr.OpenrConfig import ttypes as openr_config_types
-from openr.py.openr.utils.ipnetwork import sprint_addr
-from openr.thrift.Network import types as network_types_py3
-from openr.thrift.Network.thrift_types import IpPrefix
-from openr.Types import ttypes as openr_types
-
-
-def sprint_prefix(
-    prefix: IpPrefix | network_types_py3.IpPrefix | network_types.IpPrefix,
-) -> str:
-    """
-    :param prefix: network_types.IpPrefix representing an CIDR network
-
-    :returns: string representation of prefix (CIDR network)
-    :rtype: str or unicode
-    """
-
-    return f"{sprint_addr(prefix.prefixAddress.addr)}/{prefix.prefixLength}"
 
 
 def ip_str_to_addr_py(
@@ -65,31 +47,4 @@ def ip_str_to_prefix_py(prefix_str: str) -> network_types.IpPrefix:
     ip_str, ip_len_str = prefix_str.split("/")
     return network_types.IpPrefix(
         prefixAddress=ip_str_to_addr_py(ip_str), prefixLength=int(ip_len_str)
-    )
-
-
-def sprint_prefix_type(prefix_type):
-    """
-    :param prefix: network_types.PrefixType
-    """
-
-    return network_types.PrefixType._VALUES_TO_NAMES.get(prefix_type, None)
-
-
-def sprint_prefix_forwarding_type(forwarding_type):
-    """
-    :param forwarding_type: openr_config_types.PrefixForwardingType
-    """
-
-    return openr_config_types.PrefixForwardingType._VALUES_TO_NAMES.get(forwarding_type)
-
-
-def sprint_prefix_forwarding_algorithm(
-    forwarding_algo: openr_config_types.PrefixForwardingAlgorithm,
-) -> str | None:
-    """
-    :param forwarding_algorithm: openr_config_types.PrefixForwardingAlgorithm
-    """
-    return openr_config_types.PrefixForwardingAlgorithm._VALUES_TO_NAMES.get(
-        forwarding_algo
     )
